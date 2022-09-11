f84f8a7431ac9850f9d743e0a9b25de0d4e67db4
jojimanos <manosgeorge7@gmail.com>
2022-09-11 19:40:47 +0300

    Pet.test.js

--- __tests__/Pet.test.js
+++ __tests__/Pet.test.js
@@ -4,10 +4,21 @@
 
 import { expect, test } from "@jest/globals";
 import { render } from "@testing-library/react";
+import { StaticRouter } from "react-router-dom/server";
 import Pet from "../src/Pet";
-import { StaticRouter } from "react-router-dom"
 
 test("displays a default thumbnail", async () => {
+    const pet = render(
+        <StaticRouter>
+            <Pet />
+        </StaticRouter>
+    );
+
+    const petThumbnail = await pet.findByTestId("thumbnail");
+    expect(petThumbnail.src).toContain("none.jpg");
+});
+
+test("displays a non-default thumbnail", async () => {
     const pet = render(
         <StaticRouter>
             <Pet images={["1.jpg", "2.jpg", "3.jpg"]} />
@@ -16,4 +27,4 @@ test("displays a default thumbnail", async () => {
 
     const petThumbnail = await pet.findByTestId("thumbnail");
     expect(petThumbnail.src).toContain("1.jpg");
-}); 
+});
